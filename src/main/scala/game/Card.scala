9c34e0768976fb7924f3de358373ce9dbff6f32a
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-06-06 20:11:02 +0100

    initial commit

---
+++ src/main/scala/game/Card.scala
@@ -0,0 +1,7 @@
+package game
+
+class Card (var symbol : String, var suit : String, var value : Int) {
+    def getSymbol : String = symbol;
+    def getSuit : String = suit;
+    def getValue : Int = value;
+}
---
+++ src/main/scala/game/CardGame.scala
@@ -0,0 +1,8 @@
+package game
+
+import scala.collection.mutable.ListBuffer
+
+class CardGame (var deckOfCards : ListBuffer[Card]) {
+    def getDeckOfCards: ListBuffer[Card] = deckOfCards;
+    def CreateDeck:
+}
